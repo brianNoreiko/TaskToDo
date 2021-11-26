1a9f6be0cfaa9e0869cbe289e8982ab061cf3e2c
ianbpn <ianb.782@live.com>
2021-11-26 12:46:19 -0300

    boardlist/views/url change

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.core.serializers import serialize
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView, ListView
@@ -48,16 +50,28 @@ class deleteTask(LoginRequiredMixin, DeleteView):
         return self.model.objects.filter(user=owner)
 
 
-class BoardsListView(ListView):
-    template_name = 'tasks/boardList.html'
+class BoardsListView(ListView,LoginRequiredMixin):
+    model = Board
+    context_object_name = 'boards'
 
-    def get_queryset(self):
-        self.user = get_object_or_404(User, name=self.kwargs['usuario'])
-        return Board.objects.filter(user=self.user)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['boards'] = context['boards'].filter(user=self.request.user)
+        return context
+
+
+    def jsonTasks(request, id):
+        data = serialize("json", Task.objects.filter(user=id))
+        return JsonResponse(data, status=200, safe=False)
+
+    #template_name = 'tasks/boardList.html'
+    #def get_queryset(self):
+    #    self.user = get_object_or_404(User, name=self.kwargs[''])
+    #    return Board.objects.filter(user=self.user)
 
 
 class CreateBoard(CreateView):
     template_name = 'tasks/boardForm.html'
     model = Board
     fields = ['name']
-    success_url = 'boards'
+    success_url = 'boardList.html'
